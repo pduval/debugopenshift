3a532869d6b2d496c214174b4d1913e6de0127cc
Philippe Duval <philippe.duval@qms-software.com>
2017-03-19 21:45:57 +0000

    more data

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
+import os
 def application(environ, start_response):
-    data = "\n".join(["{0}: {1}".format(key,val) for (key,val) in environ.iteritems()])
+    req_data = "\n".join(["{0}: {1}".format(key,val) for (key,val) in environ.iteritems()])
+    sys_data = "\n".join(["{0}: {1}".format(key,val) for (key,val) in os.environ.iteritems()])
+    data = "Request Data:\n{0}\n\nEnvironment:\n{1}\n\n".format(req_data, sys_data)
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
